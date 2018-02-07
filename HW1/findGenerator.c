1f5663ca146cddbb6192d71f9a4f9734eeefcd54
AJ <taj95@vt.edu>
2018-02-06 21:51:22 -0500

    Updated HW

--- HW1/findGenerator.c
+++ HW1/findGenerator.c
@@ -1,6 +1,8 @@
-//Finds a random generator of the given prime number
+//Finds the first generator of the given prime number
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
 int findGen(int);
 
@@ -10,10 +12,26 @@ void main() {
 	printf("Enter a prime number: ");
 	scanf("%d", &x);
 
-	int out = findGen(int x);
+	int out = findGen(x);
 	printf("%d is a generator of Z_%d.\n", out, x);
 }
 
 int findGen(int x) {
-
+	bool found;
+	for (int g = 2; g<x; g++) {
+		found = true;
+		for (int r=1; r<x-1; r++) {
+			int res = (long)pow((double) g, (double) r)%x;
+			printf("g: %d, r: %d, res: %d\n", g, r, res);
+			if (res==1) {
+				//printf("%d"pow(g,r));
+				found = false;
+				break; //g is not a generator so skip to the next
+			}
+		}
+		if (found) {
+			return g;
+		}
+	}
+	return -1; //nothing was found
 }
