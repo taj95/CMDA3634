908f6285bde303fa0e65852b0e0f7d63003043ea
AJ <taj95@vt.edu>
2018-03-13 15:38:21 -0400

    Class Exercise

--- L11/spi.c
+++ L11/spi.c
@@ -3,31 +3,31 @@
 #include <math.h>
 
 int main(int argc, char **argv) {
+	//need running tallies
+	long long int Ntotal;
+	long long int Ncircle;
 
-  //need running tallies
-  long long int Ntotal;
-  long long int Ncircle;
+	//seed RNG
+	double seed = 1.0;
+	srand48(seed);
+	
+	for (long long int n=0; n<1000000; n++) {
+		//generate two random numbers
+		double rand1 = drand48(); //drand48 returns a number between 0 and 1
+		double rand2 = drand48();
 
-  //seed random number generator
-  double seed = 1.0;
-  srand48(seed);
+		double x = -1 + 2*rand1; //shift to [-1,1]
+		double y = -1+2*rand2;
 
-  for (long long int n=0; n<1000000000;n++) {
-    //gererate two random numbers
-    double rand1 = drand48(); //drand48 returns a number between 0 and 1
-    double rand2 = drand48();
-    
-    double x = -1 + 2*rand1; //shift to [-1,1]
-    double y = -1 + 2*rand2;
+		//check if its in the circle
+		if (sqrt(x*x+y*y)<=1) Ncircle++;
+		Ntotal++;
+	}
 
-    //check if its in the circle
-    if (sqrt(x*x+y*y)<=1) Ncircle++;
-    Ntotal++;
-  }
+	double pi = 4.0*Ncircle/(double) Ntotal;
 
-  double pi = 4.0*Ncircle/ (double) Ntotal;
+	printf("Our estimate of pi is %f \n", pi);
 
-  printf("Our estimate of pi is %f \n", pi);
+	return 0;
 
-  return 0;
 }
